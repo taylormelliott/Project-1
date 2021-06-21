73e1760977a03fabe69a955be690f5c2d20fdce4
Taylor Elliott <tmikale@gmail.com>
2021-06-21 12:35:13 -0600

    Rough Draft

---
+++ index.js
@@ -0,0 +1,47 @@
+const subitBtn = document.querySelector("#submitBtn");
+const question = document.querySelector("#question");
+
+let magicBallResponse = () => {
+  let randomNumber = Math.floor(Math.random() * 8);
+  switch (randomNumber) {
+    case 0:
+      eightBall = "It is certain";
+      break;
+    case 1:
+      eightBall = "It is decidedly so";
+      break;
+    case 2:
+      eightBall = "Reply hazy try again";
+      break;
+    case 3:
+      eightBall = "Cannot predict now";
+      break;
+    case 4:
+      eightBall = "Do not count on it";
+      break;
+    case 5:
+      eightBall = "My sources say no";
+      break;
+    case 6:
+      eightBall = "Outlook not so good";
+      break;
+    case 7:
+      eightBall = "Signs point to yes";
+      break;
+  }
+  return eightBall;
+};
+
+let getResponse = (e) => {
+  e.preventDefault();
+
+  let response = document.createElement("h1");
+  response.className = "align-response";
+  response.textContent = `Magic Eight Ball Says: ${magicBallResponse()}`;
+  document.body.appendChild(response);
+  setTimeout(() => {
+    question.value = "";
+    response.remove();
+  }, 3000);
+};
+subitBtn.addEventListener("click", getResponse);
